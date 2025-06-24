69bd0a7949c3a9c22db09fe63c6c81fcc047438c
callum7034 <dr.awesomeyolo@gmail.com>
2025-06-24 20:18:49 +1200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        
+        document.querySelector('.nav-links').classList.remove('active');
+    });
+});
